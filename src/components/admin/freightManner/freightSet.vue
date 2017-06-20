6a935bf7b05f7a70be08d90c4dfc98144e5cff0e
刘召阳 <xiao_yu_hn@163.com>
2017-06-20 11:22:46 +0800

    邮费设置（2）

--- src/components/admin/freightManner/freightSet.vue
+++ src/components/admin/freightManner/freightSet.vue
@@ -1,14 +1,53 @@
 <template>
-  <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
-    <el-tab-pane label="自定义邮费" name="first">自定义邮费</el-tab-pane>
-    <el-tab-pane label="包邮" name="fourth">包邮</el-tab-pane>
-  </el-tabs>
+  <div class="freightSet">
+    <el-button>修改运费</el-button>
+    <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+      <el-tab-pane label="自定义邮费" name="first">
+        <el-table  :data="tableData"  stripe  style="width: 100%">
+          <el-table-column  prop="places" align="center"  label="运送到" >
+            <template scope="scope">
+              <span v-if="scope.places.length>0" v-for="place in places">{{scope.rows.place}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column  prop="firstNum" align="center"  label="首件数（件）"  width="200">
+            <template scope="scope">
+              <el-input></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column  prop="firstPay" align="center"  label="首费（元）"  width="200">
+            <template scope="scope">
+              <el-input></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column  prop="moreNum" align="center"  label="续件数（件）"  width="200">
+            <template scope="scope">
+              <el-input></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column  prop="morePay" align="center"  label="续费（元）"  width="200">
+            <template scope="scope">
+              <el-input></el-input>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="包邮" name="fourth">
+
+
+
+    这是邮费</el-tab-pane>
+    </el-tabs>
+
+  </div>
 </template>
 <script>
   export default {
     name: 'freightSet',
     data () {
       return {
+        tableData:[
+          {}
+        ],
         activeName2: 'first'
       }
     },
