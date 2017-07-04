1512132c0d5ab471df8b14793428a611d5b11375
王鑫 <xin_web@sina.com>
2017-07-04 09:38:10 +0800

    视频管理

--- src/components/admin/videoManner/video.vue
+++ src/components/admin/videoManner/video.vue
@@ -9,9 +9,11 @@
 
     <!--视频列表-->
     <el-table :data="videoList" border>
-      <el-table-column prop="video_id" label="视频" :span="12" align="center">
+      <el-table-column prop="vidName" label="视频" :span="8" align="center">
       </el-table-column>
-      <el-table-column prop="handle" label="操作" :span="12" align="center">
+      <el-table-column prop="vidRoute" label="链接" :span="8" align="center">
+      </el-table-column>
+      <el-table-column prop="handle" label="操作" :span="8" align="center">
         <template scope="scope">
           <el-button
             size="small"
@@ -42,58 +44,151 @@
 		    	<el-input v-model="video_link" class="fl" style="width:200px;padding-right:20px;" :placeholder="ifPlaceHolder"></el-input>
 		      <el-upload
 					  class="upload-demo"
-					  ref="upload"
-					  action="https://jsonplaceholder.typicode.com/posts/"
-					  :on-preview="handlePreview"
-					  :on-remove="handleRemove"
-					  :file-list="fileList"
-					  :auto-upload="false">
+					  :action="qiNiuUrl"
+					  :on-success="uploadFile"
+					  :data="qiNiuToken"
+					  :file-list="fileList">
 					  <el-button slot="trigger" type="primary" v-if="radio==='1'" >选择</el-button>
 					</el-upload>
 		    </li>
 		  </ul>
     <div style="margin-top:30px;">
-      <el-button class="btn_" type="primary">保存</el-button>
+      <el-button class="btn_" type="primary" @click="saveVideo">保存</el-button>
     </div>
     </el-dialog>
   </el-row>
 </template>
 
 <script>
+	import global from '../../global/global'
 	export default {
 		data(){
 			return {
 				videoList: [{
-					video_id: 'xxxxxxx'
+					vidName: 'xxxxxxx'
 				}],
+				qiNiuToken: null,
+        qiNiuUrl: global.qiNiuUrl,
 				videoVisible: false,
 				radio: '1',
+				viId: '',
 				input_video: '',
 				video_link: '',
 				isEdit: false,
-				fileList: [{name: 'food.mp4', url: ''}]
+				fileList: []
 			}
 		},
+		created(){
+      //获取七牛token
+      global.axiosGetReq('/file/getUpToken', null).then((res) => {
+        if (res.data.callStatus === 'SUCCEED') { 
+          this.qiNiuToken = {
+            token: res.data.msg
+          }
+        }
+      })
+      this.queryHandler();
+    },
 		computed: {
 			ifPlaceHolder: function(){
 				return this.radio === '1' ? this.ifPlaceHolder = '' : this.ifPlaceHolder = '请输入视频地址'
 			}
 		},
 		methods: {
+			queryHandler(){
+				global.axiosPostReq('/vid/showVid',{}).then((res) => {
+          if (res.data.callStatus === 'SUCCEED') { 
+            this.videoList = res.data.data
+          }else{
+            this.$message.error('查询失败！');
+          }
+        })
+			},
 			videoAddHandler(){
 				this.videoVisible = true;
 				this.isEdit = false;
 				this.input_video = '';
+				this.video_link = '';
+				this.fileList = [];
+			},
+			uploadFile(res, file) {
+        this.video_link = global.qiniuShUrl + file.response.key
+        this.fileList = [];
+        this.fileList.push({
+        	name: this.video_link,
+        	url: this.video_link
+        })
+      },
+			saveVideo(){
+				if(!this.isEdit){
+					let params = {
+						vidName: this.input_video,
+						videoType: this.radio,
+						vidRoute: this.video_link
+					}
+	        global.axiosPostReq('/vid/insertVid',params).then((res) => {
+	          if (res.data.callStatus === 'SUCCEED') { 
+	            this.$message({
+	              message: '添加成功',
+	              type: 'success'
+	            });
+	            this.fileList = [];
+	            this.videoVisible = false;
+	            this.queryHandler();
+	          }else{
+	            this.$message.error('添加失败！');
+	          }
+	        })
+	        
+				}else{
+					let params = {
+						viId: this.viId,
+						vidName: this.input_video,
+						vidType: this.radio,
+						vidRoute: this.video_link
+					}
+	        global.axiosPostReq('/vid/updateVid',params).then((res) => {
+	          if (res.data.callStatus === 'SUCCEED') { 
+	            this.$message({
+	              message: '修改成功',
+	              type: 'success'
+	            });
+	            this.fileList = [];
+	            this.videoVisible = false;
+	            this.queryHandler();
+	          }else{
+	            this.$message.error('修改失败！');
+	          }
+	        })
+	        
+				}
+				
 			},
 			handleEdit(index, row){
 				this.videoVisible = true;
 				this.isEdit = true;
-				this.input_video = row.video_id;
+				this.input_video = row.vidName;
+				this.radio = row.videoType;
+				this.video_link = row.vidRoute,
+				this.viId = row.viId
 			},
 			handleDel(index, row){
 				this.$confirm('确定删除吗？')
           .then(_ => {
-            done();
+            let params = {
+							viId: row.viId
+						}
+		        global.axiosPostReq('/vid/deleteVid',params).then((res) => {
+		          if (res.data.callStatus === 'SUCCEED') { 
+		            this.$message({
+		              message: '删除成功',
+		              type: 'success'
+		            });
+		            this.queryHandler();
+		          }else{
+		            this.$message.error('删除失败！');
+		          }
+		        })
           })
           .catch(_ => {});
 			},
